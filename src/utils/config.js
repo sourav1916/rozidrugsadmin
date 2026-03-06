ad9cc6e61b86be7a693b23fc4deb96dace9ad936
sourav1916 <souravadhikary1916@gmail.com>
2026-03-06 22:46:47 +0530

    Changed backend url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_API_URL = "http://localhost:9909/admin";
+// export const BASE_API_URL = "http://localhost:9909/admin";
+export const BASE_API_URL = "https://rozidrugsapi.vercel.app/admin";
