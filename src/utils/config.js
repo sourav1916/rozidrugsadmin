9add940adf15633d8cf17ddfd6fad08c96ee883f
sourav1916 <souravadhikary1916@gmail.com>
2026-03-24 17:41:17 +0530

    Changed backend endpoint

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 // export const BASE_API_URL = "http://localhost:9909/admin";
-export const BASE_API_URL = "https://rozidrugsapi.vercel.app/admin";
+export const BASE_API_URL = "https://rozimed-server.rozidrugs.in/admin";
